ed6e9189fca4f10bd5562cabdc02eda994ea256c
chaimaa elakfa <chaimaaelakfa753@gmail.com>
2023-05-17 13:39:11 +0100

    working pad numbers

--- app/src/main/java/com/example/test_your_brain_personnel/GameScreen.kt
+++ app/src/main/java/com/example/test_your_brain_personnel/GameScreen.kt
@@ -21,14 +21,46 @@ class GameScreen : AppCompatActivity() {
             val intent = Intent(this@GameScreen, FinalScreen::class.java)
             startActivity(intent)
         }
+        setupNumberButtonListeners()
+
 // Inside your activity or fragment
 
     }
+
+    private fun setupNumberButtonListeners() {
+        val numberButtons = listOf<Button>(
+            findViewById(R.id.num0),
+            findViewById(R.id.num1),
+            findViewById(R.id.num2),
+            findViewById(R.id.num3),
+            findViewById(R.id.num4),
+            findViewById(R.id.num5),
+            findViewById(R.id.num6),
+            findViewById(R.id.num7),
+            findViewById(R.id.num8),
+            findViewById(R.id.num9)
+        )
+        for (button in numberButtons) {
+            button.setOnClickListener {
+                val enteredNumber = button.text.toString()
+                appendNumberToAnswer(enteredNumber)
+            }
+        }
+    }
+
+    private fun appendNumberToAnswer(number: String) {
+        val answerTextView = findViewById<TextView>(R.id.Answer)
+        val currentAnswer = answerTextView.text.toString()
+        val newAnswer = "$currentAnswer$number"
+        answerTextView.text = newAnswer
+    }
+
     var i=0
     fun condition(){
         if(i==4){ val intent = Intent(this@GameScreen, FinalScreen::class.java)
             startActivity(intent)}
     }
+    //rqndom operations
     fun randomOperation(){
         val randNum1: TextView = findViewById(R.id.randNum1)
         val randNum2: TextView = findViewById(R.id.randNum2)
@@ -52,5 +84,6 @@ class GameScreen : AppCompatActivity() {
         operation.text = operationSymbol
 
         condition()
+
     }
 }
