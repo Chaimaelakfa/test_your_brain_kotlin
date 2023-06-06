dcdb11e2bdb5ba29d8f4925f38d6e8101bcb6722
Nouhaila <iguerdnouhaila@gmail.com>
2023-06-07 00:45:30 +0100

    adding splach of Imane but needs animation

---
+++ app/src/main/java/com/example/test_your_brain_personnel/Splash.kt
@@ -0,0 +1,22 @@
+package com.example.test_your_brain_personnel
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this,WelcomeScreen::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
+    }
+}
